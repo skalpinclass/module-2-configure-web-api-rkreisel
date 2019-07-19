05bfbb9d41360916491837fba08ddfc288a6df47
Randy Kreisel <randy@rkreisel.com>
2019-07-19 15:25:26 -0500

    tweaks

--- Blog.WebApi/Controllers/BlogController.cs
+++ Blog.WebApi/Controllers/BlogController.cs
@@ -11,37 +11,53 @@ namespace Blog.WebApi.Controllers
     public class BlogController : ControllerBase
     {
         private BlogContext db;
-        private JsonSerializerSettings jsonSettings;
-        private JsonSerializerSettings jsonSettingsIndented;
+        private readonly JsonSerializerSettings jsonSettings;
 
         public BlogController(BlogContext db)
         {
             this.db = db;
             this.jsonSettings = new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects };
-            this.jsonSettingsIndented = new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects, Formatting = Formatting.Indented };
         }
 
         [HttpGet]
         [Produces("application/json")]
         [ProducesResponseType(200)]
-        public ActionResult<IEnumerable<Blog>> Get(bool includePosts = true, bool formatted = false)
+        //public ActionResult<IEnumerable<Blog>> Get(bool includePosts = true)
+        public ActionResult<IEnumerable<Blog>> Get(bool includePosts = true)
         {
             IEnumerable<Blog> result;
+            var query = db.Blogs;
             if (includePosts)
-                result = db.Blogs.Include(blog => blog.Posts).AsEnumerable();
+                result = query.Include(blog => blog.Posts).AsEnumerable();
             else
-                result = db.Blogs.AsEnumerable();
+                result = query.AsEnumerable();
+            return new JsonResult(result, this.jsonSettings);
+        }
 
-            if (formatted)
-                return Ok(JsonConvert.SerializeObject(result, this.jsonSettingsIndented));
+        [HttpGet("{id}")]
+        [Produces("application/json")]
+        [ProducesResponseType(200)]
+        public ActionResult<Blog> Get(int id, bool includePosts = true)
+        {
+            Blog result;
+            IEnumerable<Blog> query;
+            if (includePosts)
+                query = db.Blogs.Include(blog => blog.Posts);
             else
-                return Ok(JsonConvert.SerializeObject(result, this.jsonSettings));
+                query = db.Blogs;
+
+            result = query.SingleOrDefault(b => b.BlogId == id);
+
+            if (result == null)
+                return NotFound($"Id {id} not found");
+
+            return new JsonResult(result, this.jsonSettings);
         }
 
         [HttpPost]
         [Produces("application/json")]
         [ProducesResponseType(200)]
-        public void Post([FromBody] Blog value)
+        public ActionResult Post([FromBody] Blog value)
         {
             var newEntry = new Blog
             {
@@ -51,37 +67,52 @@ namespace Blog.WebApi.Controllers
             };
             db.Blogs.Add(newEntry);
             db.SaveChanges();
+            return Ok($"Blog post added. Id = {newEntry.BlogId}");
         }
 
         [HttpPut("{id}")]
         [Produces("application/json")]
         [ProducesResponseType(200)]
-        public void Put(int id, [FromBody] Blog value)
+        public ActionResult Put(int id, [FromBody] Blog value)
         {
-            var existingBlogEntry = db.Blogs.FirstOrDefault(b => b.BlogId == value.BlogId);
+            var existingBlogEntry = db.Blogs.Include(blog => blog.Posts).FirstOrDefault(b => b.BlogId == value.BlogId);
             if (existingBlogEntry == null)
             {
-                throw new InvalidOperationException($"Could not find BlogId {value.BlogId} to update.");
+                return NotFound($"Could not find BlogId {value.BlogId} to update.");
             }
-            existingBlogEntry.Posts = value.Posts;
+            foreach (var incomingPost in value.Posts)
+            {
+                var existingPost = existingBlogEntry.Posts.SingleOrDefault(p => p.BlogPostId == incomingPost.BlogPostId);
+                if (existingPost != null)
+                {
+                    existingPost.Title = incomingPost.Title;
+                    existingPost.Content = incomingPost.Content;
+                }
+                else
+                {
+                    existingBlogEntry.Posts.Add(new BlogPost { Content = incomingPost.Content, Title = incomingPost.Title, BlogId = existingBlogEntry.BlogId });
+                }
+            }
             existingBlogEntry.Rating = value.Rating;
             existingBlogEntry.Url = value.Url;
             db.Blogs.Update(existingBlogEntry);
             db.SaveChanges();
+            return Ok($"Blog with id of {id} updated.");
         }
 
         [HttpDelete("{id}")]
         [ProducesResponseType(200)]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
             var existingBlogEntry = db.Blogs.FirstOrDefault(b => b.BlogId == id);
             if (existingBlogEntry == null)
             {
-                throw new InvalidOperationException($"Could not find BlogId {id} to delte.");
+                return NotFound($"Could not find BlogId {id} to delete.");
             }
             db.Blogs.Remove(existingBlogEntry);
             db.SaveChanges();
+            return Ok($"Blog with id of {id} was deleted.");
         }
-        
+
     }
 }
